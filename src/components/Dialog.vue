118157fe3dcee986b656874581c282f33cc54791
Alibek-dev <54788352+Alibek-dev@users.noreply.github.com>
2021-04-01 15:07:37 +0600

    добавлено контекстное меню

---
+++ src/components/Dialog.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-dialog
+        v-model="dialog"
+        persistent
+        max-width="400"
+    >
+        <v-card>
+            <v-card-title class="headline">
+                Вы действительно хотите удалить?
+            </v-card-title>
+            <v-card-text>Сотрудника <strong>{{ itemContextMenu.fullName }}</strong></v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn
+                    color="green darken-1"
+                    text
+                    @click="dialog = false"
+                >
+                    Disagree
+                </v-btn>
+                <v-btn
+                    color="green darken-1"
+                    text
+                    @click="dialog = false"
+                >
+                    Agree
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+</template>
+
+<script>
+export default {
+    name: "Dialog",
+    props: {
+        itemContextMenu: Object,
+        dialog: Boolean
+    },
+
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/components/EmployeeTable.vue
+++ src/components/EmployeeTable.vue
@@ -11,10 +11,53 @@
             <v-btn class="ml-10">Добавить</v-btn>
         </v-card-title>
         <v-data-table
+            @click:row="rowClick"
+            @contextmenu:row="rowContextMenu"
+            item-key="fullName"
+            single-select
             :headers="headers"
             :items="employees"
             :search="search"
-        ></v-data-table>
+        >
+
+        </v-data-table>
+
+        <v-menu v-model="showMenu" :position-x="x" :position-y="y" absolute offset-y>
+            <v-list>
+                    <v-btn @click="dialog = !dialog" depressed><v-icon>mdi-delete</v-icon> Удалить</v-btn>
+            </v-list>
+        </v-menu>
+
+        <v-dialog
+            v-model="dialog"
+            persistent
+            max-width="400"
+        >
+            <v-card>
+                <v-card-title class="headline">
+                    Вы действительно хотите удалить?
+                </v-card-title>
+                <v-card-text>Сотрудника <strong>{{ itemContextMenu.fullName }}</strong></v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="dialog = false"
+                    >
+                        Disagree
+                    </v-btn>
+                    <v-btn
+                        color="green darken-1"
+                        text
+                        @click="dialog = false"
+                    >
+                        Agree
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+
     </v-card>
 </template>
 
@@ -22,9 +65,20 @@
 export default {
     name: "Table",
     props: {
-        employees: []
+        employees: Array,
     },
     data: () => ({
+        selectedId: -1,
+        showMenu: false,
+        x: 0,
+        y: 0,
+        contextMenuItems: [
+            {title: 'Удалить'}
+        ],
+
+        itemContextMenu: {},
+        dialog: false,
+
         search: '',
         headers: [
             {text: 'ФИО', align: 'start', value: 'fullName'},
@@ -32,10 +86,34 @@ export default {
             {text: 'Общая стоимость', align: 'start', value: 'sumOfTangiblesValue'},
 
         ]
-    })
+    }),
+    methods: {
+        rowClick: function (item, row) {
+            if (this.selectedId === item.fullName) {
+                this.selectedId = -1
+                row.select(false)
+            } else {
+                this.selectedId=item.fullName
+                row.select(true);
+            }
+        },
+        rowContextMenu: function (e, item) {
+            document.oncontextmenu = function (){return false}
+            this.itemContextMenu = item.item
+
+            this.showMenu = false;
+            this.x = e.clientX;
+            this.y = e.clientY;
+            this.$nextTick(() => {
+                this.showMenu = true;
+            });
+        }
+    },
 }
 </script>
 
-<style scoped>
-
+<style>
+tr.v-data-table__selected {
+    background: salmon !important;
+}
 </style>
