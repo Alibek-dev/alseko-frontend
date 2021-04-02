0191ccc00362c442bae1581214e74bfb3586026a
Alibek-dev <54788352+Alibek-dev@users.noreply.github.com>
2021-04-03 03:26:12 +0600

    Добавлена валидация в форму материальных ценностей

--- src/components/TangibleForm.vue
+++ src/components/TangibleForm.vue
@@ -12,6 +12,9 @@
                         placeholder="Введите название материальной ценности"
                         outlined
                         v-model="subject"
+                        counter="50"
+                        :error="$v.subject.$invalid && $v.subject.$error"
+                        @input="$v.subject.$touch()"
                     ></v-text-field>
                 </v-col>
                 <v-col cols="12">
@@ -20,6 +23,8 @@
                         placeholder="Введите стоимость"
                         outlined
                         v-model="price"
+                        :error="$v.price.$invalid && $v.price.$error"
+                        @input="$v.price.$touch()"
                     ></v-text-field>
                 </v-col>
             </v-container>
@@ -46,6 +51,7 @@
 </template>
 
 <script>
+import {required, maxLength, maxValue, minValue, numeric} from 'vuelidate/lib/validators'
 export default {
     name: "TangibleForm",
     props: {
@@ -57,25 +63,31 @@ export default {
         subject: '',
         price: '',
     }),
-    mounted() {
-
-        console.log(this.editedItem)
+    validations: {
+        subject: {required, maxLength: maxLength(50)},
+        price: {required, numeric, minValue: minValue(1), maxValue: maxValue(10000000)}
     },
     watch: {
         showTangibleForm() {
             this.subject = this.editedItem.subject
             this.price = this.editedItem.price
             console.log(this.editedItem)
+            this.$v.$reset()
         },
     },
     methods: {
         submit() {
-            this.$emit('save', {
-                    subject: this.subject,
-                    price: this.price
-                }
-            )
-            console.log('Сохранить')
+            this.$v.$touch()
+            if (this.$v.$invalid) {
+                console.log('Не пройдёшь')
+            } else {
+                this.$emit('save', {
+                        subject: this.subject,
+                        price: this.price
+                    }
+                )
+            }
+
         }
     }
 
